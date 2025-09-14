605e9ff6cc327cbcb468ec69411d8bfda6342cfa
MrmAya <kahlimeriemaya@gmail.com>
2025-09-14 18:01:30 +0200

    routes pour encryption/decryption RSA

--- routes/modern.py
+++ routes/modern.py
@@ -2,7 +2,9 @@ from flask import Flask, request, jsonify, Blueprint
 from crypto.modern.aes import aes_encrypt_ECB, aes_decrypt_ECB, aes_encrypt_CBC, decrypt_AES_CBC
 from crypto.modern.rc4 import rc4
 from crypto.modern.des import des_encrypt, des_decrypt
+
 import base64
+from crypto.modern.rsa import generate_keys, encrypt, decrypt
 
 modern_bp = Blueprint("Modern", __name__)
 
@@ -19,7 +21,7 @@ def aes_encrypt_route():
             "iv": base64.b64encode(iv).decode(), 
             "ciphertext": base64.b64encode(ciphertext).decode()  
         })
-    else:  # ECB
+    else:  #ecb
         ciphertext = aes_encrypt_ECB(text, key)
         return jsonify({
             "ciphertext": base64.b64encode(ciphertext).decode()
@@ -93,3 +95,61 @@ def des_decrypt_route():
     return jsonify({
         "plaintext": pt.decode("utf-8")
     })
+
+@modern_bp.route('/rsa/encrypt', methods=['POST'])
+def rsa_encrypt_route():
+    try:
+        data = request.get_json()
+        text = data.get("text")
+        bits = data.get("bits", 1024)
+        n = data.get("n")
+        p = data.get("p")
+        q = data.get("q")
+        e = data.get("e")
+        
+        if not text :
+            return jsonify({"error": "Text Field is required ! "})
+        
+        pub, priv = generate_keys(n=n, p=p, q=q, e=e, bits=bits)
+        ciphertext = encrypt(text, pub)
+        return jsonify({
+            "public_key": {"n": pub[0], "e": pub[1]},
+            "private_key": {"n": priv[0], "d": priv[1]},
+            "ciphertext": ciphertext
+        })
+    except Exception as ex:
+        return jsonify({"error": str(ex)}), 400
+    
+
+@modern_bp.route('/rsa/decrypt', methods=['POST'])
+def rsa_decrypt_route():
+    try:
+        data = request.get_json(force=True)
+        blocks = data.get("blocks")
+        priv = data.get("priv")
+        p = data.get("p")
+        q = data.get("q")
+        e = data.get("e")
+
+        if not blocks:
+            return jsonify({"error": "Le champ 'blocks' est requis."}), 400
+
+        if priv:
+            priv_tuple = (priv["n"], priv["d"])
+        else:
+            priv_tuple = None
+
+        plaintext = decrypt(
+            blocks,
+            priv=priv_tuple,
+            p=p,
+            q=q,
+            e=e
+        )
+
+        return jsonify({
+            "plaintext": plaintext
+        })
+
+    except Exception as ex:
+        return jsonify({"error": str(ex)}), 400
